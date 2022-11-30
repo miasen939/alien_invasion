51c2ef5cc58cc9c88ec93f55fca6aa801d51ce11
miasen <miasen939@foxmail.com>
2022-11-30 13:28:52 +0800

    create a ship class

---
+++ ship.py
@@ -0,0 +1,20 @@
+import pygame
+
+class Ship():
+    """a class of Ship in game"""
+    def ___init__(self,screen):
+        """init ship and set location"""
+        self.screen = screen
+
+        #load ship image(.bmp) and get rectangle
+        self image = pygame.image,load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        #set new ship on the center bottom of screen
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+    def blitme:
+        """blit the ship at the pointed position"""
+        self.screen.blit(self.image,self.rect) 
